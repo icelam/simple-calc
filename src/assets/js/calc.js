8e5c4d60428d054eb1f26fe6f81eaa709fb2ee9a
Ice Lam <pinky.yy.lam@gmail.com>
2019-04-08 11:23:50 +0800

    Minor bug fixes

--- src/assets/js/calc.js
+++ src/assets/js/calc.js
@@ -1,4 +1,4 @@
-const simpleCalculator = () => {
+const SimpleCalculator = () => {
   /* Constants */
   const resultAreaEl = document.querySelector('.js-calculator-result');
 
@@ -10,9 +10,21 @@ const simpleCalculator = () => {
 
   /* Variables */
   let equationFinish = true;
-  let allowNextZero = false;
 
   /* Functions */
+  const _getLastInput = () => {
+    const re = new RegExp(`[\\${symbolAdd}|\\${symbolSubtract}|\\${symbolMultiply}|\\${symbolDivide}]`);
+    const inputNumbers = resultAreaEl.value.split(re);
+    return inputNumbers[inputNumbers.length - 1];
+  };
+
+  const _lastIsNotSymbol = () => {
+    const lastInput = _getLastInput();
+    const re = new RegExp(`^[\\${symbolAdd}|\\${symbolSubtract}|\\${symbolMultiply}|\\${symbolDivide}]$`);
+
+    return lastInput.length && !re.test(lastInput);
+  };
+
   const _displayResult = (val) => {
     resultAreaEl.value = val;
     equationFinish = true;
@@ -23,60 +35,72 @@ const simpleCalculator = () => {
   };
 
   const _buttonClick = (val) => {
-    //check if 0 is allowed
-    if(val == symbolAdd || val == symbolSubtract || val == symbolMultiply || val == symbolDivide) {
-      allowNextZero = false;
-    } else {
-      allowNextZero = true;
-    }
+    const lastInput = _getLastInput();
 
     // New calculation
     if (equationFinish) {
-      if (val != symbolAdd && val != symbolSubtract && val != symbolMultiply && val != symbolDivide && val != symbolDot) {
+      if (val !== symbolAdd && val !== symbolSubtract && val !== symbolMultiply && val !== symbolDivide && val !== symbolDot) {
         resultAreaEl.value = val;
+        equationFinish = true;
       } else {
         resultAreaEl.value += val;
+        equationFinish = false;
       }
     } else {
-      resultAreaEl.value += val;
-    }
+      // handling leading zeros
+      if (lastInput === '0' && val !== symbolAdd && val !== symbolSubtract && val !== symbolMultiply && val !== symbolDivide && val !== symbolDot) {
+        const displayEquation = resultAreaEl.value;
+        resultAreaEl.value = displayEquation.substring(0, displayEquation.length - 1);
+      }
 
-    equationFinish = false;
+      resultAreaEl.value += val;
+      equationFinish = false;
+    }
   };
 
   const _checkDot = () => {
-    if (document.querySelector('.js-calculator-result').value.indexOf(symbolDot) == -1)
+    const lastInput = _getLastInput();
+
+    if (lastInput.indexOf(symbolDot) === -1) {
       _buttonClick(symbolDot);
+    }
   };
 
-  const _checkZero = () => {
-    if (allowNextZero)
-      _buttonClick('0');
+  const _checkSymbol = (val) => {
+    if (!_lastIsNotSymbol()) {
+      const displayEquation = resultAreaEl.value;
+      resultAreaEl.value = displayEquation.substring(0, displayEquation.length - 1);
+    }
+
+    _buttonClick(val);
   };
 
   const _calculateResult = () => {
-    try {
-      /****** To-fix: https://stackoverflow.com/questions/588004/is-floating-point-math-broken ******/
-      const calculationResult = eval(document.querySelector('.js-calculator-result').value);
-      // Do not round
-      _displayResult(calculationResult);
+    if (_lastIsNotSymbol()) {
+      try {
+        /* To-fix: https://stackoverflow.com/questions/588004/is-floating-point-math-broken */
+        const calculationResult = eval(resultAreaEl.value);
 
-      // Round Result to nearest 9 dp
-      // const roundedResult = Math.round(calculationResult * 1000000000) / 1000000000
-      // _displayResult(roundedResult);
-    } catch (e) {
-      _displayResult('Error');
-      if (window.console) {
-        console.log(e);
+        // Do not round
+        _displayResult(calculationResult);
+
+        // Round Result to nearest 9 dp
+        // const roundedResult = Math.round(calculationResult * 1000000000) / 1000000000
+        // _displayResult(roundedResult);
+      } catch (e) {
+        _displayResult('Error');
+        if (window.console) {
+          console.log(e);
+        }
       }
-    }
 
-    equationFinish = true;
+      equationFinish = true;
+    }
   };
 
   /* Event Listeners */
   const _init = () => {
-    document.querySelector('.js-button-0').addEventListener('click', _checkZero);
+    document.querySelector('.js-button-0').addEventListener('click', () => _buttonClick('0'));
     document.querySelector('.js-button-1').addEventListener('click', () => _buttonClick('1'));
     document.querySelector('.js-button-2').addEventListener('click', () => _buttonClick('2'));
     document.querySelector('.js-button-3').addEventListener('click', () => _buttonClick('3'));
@@ -87,23 +111,26 @@ const simpleCalculator = () => {
     document.querySelector('.js-button-8').addEventListener('click', () => _buttonClick('8'));
     document.querySelector('.js-button-9').addEventListener('click', () => _buttonClick('9'));
 
-    document.querySelector('.js-button-add').addEventListener('click', () => _buttonClick(symbolAdd));
-    document.querySelector('.js-button-subtract').addEventListener('click', () => _buttonClick(symbolSubtract));
-    document.querySelector('.js-button-multiply').addEventListener('click', () => _buttonClick(symbolMultiply));
-    document.querySelector('.js-button-divide').addEventListener('click', () => _buttonClick(symbolDivide));
+    document.querySelector('.js-button-add').addEventListener('click', () => _checkSymbol(symbolAdd));
+    document.querySelector('.js-button-subtract').addEventListener('click', () => _checkSymbol(symbolSubtract));
+    document.querySelector('.js-button-multiply').addEventListener('click', () => _checkSymbol(symbolMultiply));
+    document.querySelector('.js-button-divide').addEventListener('click', () => _checkSymbol(symbolDivide));
 
     document.querySelector('.js-button-dot').addEventListener('click', _checkDot);
     document.querySelector('.js-button-clear').addEventListener('click', _clearResult);
     document.querySelector('.js-button-equal').addEventListener('click', _calculateResult);
 
-    document.onkeyup = (e) => {
-      switch(e.which) {
-        // 0
-        case 48:
-          _checkZero();
-          break;
+    // Prevent Enter key pressing buttons
+    document.onkeydown = (e) => {
+      if (e.which === 13) {
+        e.preventDefault();
+      }
+    };
 
-        // 1 - 7, 9
+    document.onkeyup = (e) => {
+      switch (e.which) {
+        // 0 - 7, 9
+        case 48:
         case 49:
         case 50:
         case 51:
@@ -150,6 +177,9 @@ const simpleCalculator = () => {
         case 191:
           _buttonClick(symbolDivide);
           break;
+
+        default:
+          break;
       }
     };
   };
@@ -159,4 +189,4 @@ const simpleCalculator = () => {
   };
 };
 
-export default simpleCalculator;
+export default SimpleCalculator;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import '@style/calc.scss';
 
-import simpleCalculator from '@js/calc.js';
+import SimpleCalculator from '@js/calc';
 
 // Calculator init
-const calc = new simpleCalculator();
+const calc = new SimpleCalculator();
 calc.init();
 
 // PWA
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js').then(registration => {
+    navigator.serviceWorker.register('./service-worker.js').then((registration) => {
       console.log('SW registered: ', registration);
-    }).catch(registrationError => {
+    }).catch((registrationError) => {
       console.log('SW registration failed: ', registrationError);
     });
   });
